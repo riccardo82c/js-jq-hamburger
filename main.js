903af936b1a6ffa2dcbce8e19853ef1746c3d087
Riccardo Colombo <rikyregna@gmail.com>
2020-07-22 18:53:33 +0200

    add 2nd solution (thanks Alberto)

--- main.js
+++ main.js
@@ -2,7 +2,12 @@
 mostrare / nascondere il menu principale */
 
 $(".header-right :last-child").click(function () {
-  $(".hamburger-menu").show("fast");
+  // 1 modo.  utilizzo il metodo .show() per mostrare un elemento nascosto
+  /* $(".hamburger-menu").show("fast"); */
+
+  // 2 modo. utilizzo il metodo .addClass per aggiungere una classe che mi dia display block all'elemento con display none;
+
+  $(".hamburger-menu").addClass("active");
 });
 
 // prova callback - dopo .show si attiva la funzione che fa l'alert
@@ -13,5 +18,8 @@ $(".header-right :last-child").click(function () {
 }); */
 
 $("a.close").click(function () {
-  $(".hamburger-menu").hide("fast");
+  /* $(".hamburger-menu").hide("fast"); */
+  $(".hamburger-menu").removeClass(
+    "active"
+  ); /* qui rimuovo la classe .active ovvero il display block */
 });
