503943ef643e36033fcdd23e544d794dd189dafc
Riccardo Colombo <rikyregna@gmail.com>
2020-07-22 18:06:01 +0200

    redefine first selector

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 /* Hamburger menu:
 mostrare / nascondere il menu principale */
 
-$("a i").click(function () {
+$(".header-right :last-child").click(function () {
   $(".hamburger-menu").show("fast");
 });
+
+// prova callback - dopo .show si attiva la funzione che fa l'alert
+/* $(".header-right :last-child").click(function () {
+  $(".hamburger-menu").show("fast", function () {
+    alert("ciao");
+  });
+}); */
 
 $("a.close").click(function () {
   $(".hamburger-menu").hide("fast");
